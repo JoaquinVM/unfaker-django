259965076b00a9e428e50d3b945a649fd5bfef6c
Joaquin <viscafe38@gmail.com>
2020-03-25 16:06:51 -0400

    ADD

--- src/main/models.py
+++ src/main/models.py
@@ -41,7 +41,10 @@ class Noticia(models.Model):
               ('Venezuela', 'Venezuela'), ('Internacional', 'Internacional'))
     pais = models.CharField(choices=paises, default='Internacional',max_length=20)
     imagen = models.CharField(max_length=100, default='default')
-    #categoria = models.ForeignKey(Categoria, default=None, blank=False, on_delete=models.CASCADE)
+    #categoria = models.ForeignKey(Categoria, default=None, blank=False, on_delete=models.CASCADE)\
+
+    def __str__(self):
+        return self.titulo
 
 class Denuncia(models.Model):
     descripcion = models.CharField(max_length=500)
